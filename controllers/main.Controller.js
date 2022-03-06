624cb4c40322486a97a678f2eb98b316d35ce2ea
Gasvn7 <morenogaston595@gmail.com>
2022-03-06 15:21:32 -0300

    vamo newells

--- controllers/main.Controller.js
+++ controllers/main.Controller.js
@@ -2,9 +2,6 @@ const path = require('path');
 const mainController = {
     home: function (req, res) {
         res.render('home.ejs');
-    },
-    navigationBar: function (req, res) {
-        res.render('navigationBar.ejs');
     }
 }
 
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const mainController = require('../controllers/main.Controller.js')
 
 router.get('/', mainController.home);
-router.get('/barra', mainController.navigationBar);
 
 module.exports = router;
